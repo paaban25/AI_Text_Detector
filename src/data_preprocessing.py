53700e6eb28c015f2009622bec1e5c89f7ddced8
paaban25 <paaban25@gmail.com>
2023-12-31 08:04:34 +0530

    Initial commit

---
+++ src/data_preprocessing.py
@@ -0,0 +1,31 @@
+# AI_Text_Detector/src/data_preprocessing.py
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+
+def load_data(file_path):
+    df = pd.read_excel(file_path)
+    return df
+
+def preprocess_data(df):
+    label_encoder = LabelEncoder()
+    df['generated'] = label_encoder.fit_transform(df['generated'])
+    return df
+
+def split_data(df):
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], df['generated'], test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def preprocess_text_data(X_train, X_test):
+    # Handle NaN values in the text data
+    X_train = X_train.fillna('')  # Replace NaN values with an empty string
+    X_test = X_test.fillna('')
+
+    # Convert text data to numerical form using TF-IDF
+    tfidf_vectorizer = TfidfVectorizer(max_features=1000, stop_words="english")
+    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+    X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+    return X_train_tfidf, X_test_tfidf, tfidf_vectorizer
---
+++ src/train_model.py
@@ -0,0 +1,21 @@
+# AI_Text_Detector/src/train_model.py
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(f"Accuracy: {accuracy}")
+
+def save_model(model, vectorizer, model_path, vectorizer_path):
+    # Save the trained model
+    joblib.dump(model, model_path)
+
+    # Save the TF-IDF vectorizer
+    joblib.dump(vectorizer, vectorizer_path)
